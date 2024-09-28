8d8b2c1fcb0a9b0a793f57bbbb03514b078d55f9
mia16 <mahnur.imran@gmail.com>
2024-09-27 23:27:25 -0700

    added 5 rounds looped

--- rpsgame/main.js
+++ rpsgame/main.js
@@ -1,7 +1,3 @@
-let humanScore = 0
-let computerScore = 0
-let round = 1
-
 function getRandomInt(max){
     return Math.floor(Math.random() * max)
 }
@@ -32,43 +28,68 @@ function getHumanChoice(){
         return "paper"
     }
     else{
-        return "Incorrect selection. Try again."
+        console.log("Try again.")
+        return getHumanChoice()
     }
 }
 
-function playRound(humanChoice, computerChoice) {
-    if(humanChoice == computerChoice){
-        return "it's a tie!"
+function playGame(){
+    let humanScore = 0
+    let computerScore = 0
+    let round = 1
+
+    function playRound(humanChoice, computerChoice) {
+        if(humanChoice == computerChoice){
+            return "it's a tie!"
+        }
+        if(humanChoice == "rock" && computerChoice == "scissors"){
+            humanScore++
+            return "You win! Rock beats Scissors"
+        }
+        if(humanChoice == "rock" && computerChoice == "paper"){
+            computerScore++
+            return "You lose! Paper beats Rock"
+        }
+        if(humanChoice == "paper" && computerChoice == "rock"){
+            humanScore++
+            return "You win! Paper beats Rock"
+        }
+        if(humanChoice == "paper" && computerChoice == "scissors"){
+            computerScore++
+            return "You lose! Scissors beats Paper"
+        }
+        if(humanChoice == "scissors" && computerChoice == "rock"){
+            computerScore++
+            return "You lose! Rock beats Scissors"
+        }
+        if(humanChoice == "scissors" && computerChoice == "paper"){
+            humanScore++
+            return "You win! Scissors beats Paper"
+        }
     }
-    if(humanChoice == "rock" && computerChoice == "scissors"){
-        humanScore++
-        return "You win! Rock beats Scissors"
+    while(round < 6){
+        const humanSelection = getHumanChoice()
+        const computerSelection = getComputerChoice()
+        
+        let intro = `Round: ${round}`
+        console.log(intro)
+        console.log("Your choice: " + humanSelection)
+        console.log("Their choice: " + computerSelection)
+        console.log(playRound(humanSelection, computerSelection))
+        round++
     }
-    if(humanChoice == "rock" && computerChoice == "paper"){
-        computerScore++
-        return "You lose! Paper beats Rock"
+
+    if(humanScore > computerScore){
+        console.log("****You won the game!****")
     }
-    if(humanChoice == "paper" && computerChoice == "rock"){
-        humanScore++
-        return "You win! Paper beats Rock"
+    if(humanScore < computerScore){
+        console.log("****You lost the game!****")
     }
-    if(humanChoice == "paper" && computerChoice == "scissors"){
-        computerScore++
-        return "You lose! Scissors beats Paper"
+    else{
+        console.log("****The game is a tie.****")
     }
-    if(humanChoice == "scissors" && computerChoice == "rock"){
-        computerScore++
-        return "You lose! Rock beats Scissors"
-    }
-    if(humanChoice == "scissors" && computerChoice == "paper"){
-        humanScore++
-        return "You win! Scissors beats Paper"
-    }
+    let result = `Game over! Your score: ${humanScore}, Computer score: ${computerScore}`
+    console.log(result) 
 }
 
-const humanSelection = getHumanChoice()
-const computerSelection = getComputerChoice()
-
-console.log("Your choice: " + humanSelection)
-console.log("Their choice: " + computerSelection)
-console.log(playRound(humanSelection, computerSelection))
+playGame()
